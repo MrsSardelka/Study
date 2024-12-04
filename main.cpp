a1da57a7c94da7cb2dc7886daa0ef899021ed0bc
Vera <verahodykina92@gmail.com>
2024-12-04 14:06:44 +0300

    Добавлен вывод суммы введённых чисел

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "sdt.h"
 #include <iostream>
 using namespace std;
-int main()
-{ int a, b;
-cout << "¬ведите два числа: ";
- cin >> a >> b;
- return 0; }
+int main() {
+    int a, b;
+    cout << "¬ведите два числа: ";
+    cin >> a >> b;
+    cout << "—умма: " << (a + b) << endl;
+    return 0; }
